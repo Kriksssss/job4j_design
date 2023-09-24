dcbc9064f781887c724195fa340500c405682492
Kriksssss <nabibeg53@gmail.com>
2023-09-24 15:45:27 +0300

    3. Итератор четных чисел [#150 #446911]

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -14,15 +14,12 @@ public class EvenNumbersIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        while (index < data.length) {
-            if (data[index] % 2 == 0) {
-                return true;
-            } else {
-                index++;
-            }
+        while (index < data.length && data[index] % 2 != 0) {
+            index++;
         }
-        return false;
+        return index < data.length;
     }
+
 
     @Override
     public Integer next() {
