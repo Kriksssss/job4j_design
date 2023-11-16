9270aa5ef5c77f6c98ceb4b6de8eee5a4cc4620f
Kriksssss <nabibeg53@gmail.com>
2023-11-16 23:29:09 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,32 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+import java.io.FileOutputStream;
+
+import static java.lang.System.*;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            out.write("1 * 2 = 2".getBytes());
+            out.write(lineSeparator().getBytes());
+            out.write("1 * 3 = 3".getBytes());
+            out.write(lineSeparator().getBytes());
+            out.write("1 * 4 = 4".getBytes());
+            out.write(lineSeparator().getBytes());
+            out.write("1 * 5 = 5".getBytes());
+            out.write(lineSeparator().getBytes());
+            out.write("1 * 6 = 6".getBytes());
+            out.write(lineSeparator().getBytes());
+            out.write("1 * 7 = 7".getBytes());
+            out.write(lineSeparator().getBytes());
+            out.write("1 * 8 = 8".getBytes());
+            out.write(lineSeparator().getBytes());
+            out.write("1 * 9 = 9".getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
