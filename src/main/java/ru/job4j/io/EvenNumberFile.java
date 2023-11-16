6dff732b964d4ad5ef80e49b902278d074876387
Kriksssss <nabibeg53@gmail.com>
2023-11-16 23:47:18 +0300

    2.1. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,44 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("data/even.txt")) {
+
+            int read;
+            StringBuilder numberStr = new StringBuilder();
+
+            while ((read = in.read()) != -1) {
+                char character = (char) read;
+
+                if (Character.isDigit(character)) {
+                    numberStr.append(character);
+                } else if (numberStr.length() > 0) {
+                    processNumber(numberStr.toString());
+                    numberStr.setLength(0);
+                }
+            }
+
+            if (numberStr.length() > 0) {
+                processNumber(numberStr.toString());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void processNumber(String numberStr) {
+        try {
+            int number = Integer.parseInt(numberStr);
+            boolean isEven = number % 2 == 0;
+            System.out.println(number + " is even: " + isEven);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid number format in the file: " + numberStr);
+        }
+    }
+}
+
+
+
