d8ea524f2b9dc62c7c9e3e467a976199f710590b
Kriksssss <nabibeg53@gmail.com>
2023-11-20 07:25:33 +0300

    5.2. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -2,8 +2,6 @@ package ru.job4j.io;
 
 import java.io.FileOutputStream;
 
-import java.io.FileOutputStream;
-
 import static java.lang.System.*;
 
 public class ResultFile {
@@ -24,7 +22,7 @@ public class ResultFile {
             out.write("1 * 8 = 8".getBytes());
             out.write(lineSeparator().getBytes());
             out.write("1 * 9 = 9".getBytes());
-        } catch (Exception e) {
+        } catch (java.io.IOException e) {
             e.printStackTrace();
         }
     }
