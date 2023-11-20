2021a44b54a757bc3fd5cd45a3149e2270e1a914
Kriksssss <nabibeg53@gmail.com>
2023-11-20 07:27:49 +0300

    2.1. FileInputStream [#4898 #460540]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
@@ -24,7 +25,7 @@ public class EvenNumberFile {
                 processNumber(numberStr.toString());
             }
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -39,6 +40,7 @@ public class EvenNumberFile {
         }
     }
 }
+
 
 
 
