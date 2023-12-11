35d9952fed335f04c94a15292702d80f5fd4b08e
Kriksssss <nabibeg53@gmail.com>
2023-12-11 20:48:20 +0300

    2. Что такое Socket? [#4850 #464983]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,6 +1,9 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,28 +11,18 @@ public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
+                try (Socket socket = server.accept();
+                     OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-                    StringBuilder request = new StringBuilder();
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        request.append(str).append(System.lineSeparator());
-                        System.out.println(str);
-                    }
+                    String requestLine = in.readLine();
+                    System.out.println(requestLine);
 
-                    String response = "HTTP/1.1 200 OK\r\n\r\n";
-
-                    if (request.toString().contains("msg=Exit")) {
+                    if (requestLine != null && requestLine.contains("msg=Bye")) {
                         server.close();
-                    } else if (request.toString().contains("msg=Hello")) {
-                        response += "Hello.";
-                    } else {
-                        response += "What.";
                     }
 
-                    out.write(response.getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     out.flush();
                 }
             }
