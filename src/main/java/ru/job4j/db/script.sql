b59e488aed5562fdd61587b7a07d1134ffdf06bd
Kriksssss <nabibeg53@gmail.com>
2023-12-21 13:57:30 +0300

    0. Что такое база данных. [#6860]

--- src/main/java/ru/job4j/db/script.sql
+++ src/main/java/ru/job4j/db/script.sql
@@ -1,2 +1,12 @@
-DELETE FROM users
-WHERE name = 'John Doe';
+CREATE TABLE script (
+    id SERIAL PRIMARY KEY,
+    lname VARCHAR(255),
+    age INT,
+    email VARCHAR(255)
+);
+
+INSERT INTO script (lname, age, email) VALUES ('John', 25, 'john@example.com');
+
+UPDATE script SET age = 26 WHERE lname = 'John';
+
+DELETE FROM script WHERE lname = 'John';
